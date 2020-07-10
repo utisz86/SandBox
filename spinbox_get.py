5ea0bf84cb161490a134dd89d741065cee6a3920
utisz86 <soma.kormanik@outlook.com>
2020-07-11 00:20:17 +0300

    basic

---
+++ spinbox_get.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+
+class Root(Tk):
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Python Tkinter Spin Box")
+        self.minsize(640, 400)
+        
+ 
+        self.spinBox()    
+
+    def spinCallBack(self):
+        value = self.spin.get()
+        print(value)
+        
+
+    def spinBox(self):
+        self.spin = ttk.Spinbox(self, from_ = 0, to = 10, command = self.spinCallBack)
+ 
+        self.spin.grid(column = 0, row = 2)
+ 
+ 
+
+root = Root()
+root.mainloop()        
---
+++ try_pickle.py
@@ -0,0 +1,25 @@
+import pickle
+
+class Company(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+with open('company_data.pkl', 'wb') as output:
+    company1 = Company("banana", 40)
+    pickle.dump(company1, output, pickle.HIGHEST_PROTOCOL)
+
+    company2 = Company("spam", 42)
+    pickle.dump(company2, output, pickle.HIGHEST_PROTOCOL)
+
+del company1
+del company2
+
+with open('company_data.pkl', 'rb') as input:
+    company1 = pickle.load(input)
+    print(company1.name)  # -> banana
+    print(company1.value)  # -> 40
+
+    company2 = pickle.load(input)
+    print(company2.name) # -> spam
+    print(company2.value)  # -> 42
