642e48a05e2fc9804d21e4cf0e5558fb5b9a3795
utisz86 <soma.kormanik@outlook.com>
2020-07-10 22:17:37 +0300

    a

---
+++ gui_tk1.py
@@ -0,0 +1,27 @@
+# https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
+
+from tkinter import Tk, Label, Button, LEFT, RIGHT, W
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is out first GUI")
+        self.label.grid(columnspan=2, sticky=W)
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.grid(row=1)
+
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.grid(row=1, column=1)
+
+
+    def greet(self):
+        print("Greetings!")
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
+
